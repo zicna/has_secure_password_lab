9a660f90c6a5ce53f607bbf472afa7c278969362
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-05-23 18:05:12 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,24 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    # @user = User.find_by(name: params[:user][:name])
-    # return head(:forbidden) unless @user.authenticate(params[:password])
-    # session[:user_id] = @user.id
+    # raise params.inspect
+    @user = User.find_by(name: params[:user][:name])
+    
+    @user = @user.try(:authenticate, params[:user][:password])
+
+    return redirect_to(controller: 'sessions', action: 'new') unless @user
+
+    session[:user_id] = @user.id
+
+    redirect_to controller:'welcome', action: 'home'
+
   end
 
   def destroy
+    session.delete :user_id
+    redirect_to '/'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,20 +4,33 @@ class UsersController < ApplicationController
   end
 
   def create
-    if !params[:user][:name] || params[:user][:name] == ''
-      flash[:alert] = "Name must be proivided."
-      redirect_to 'login_path'
-    else
-        @user = User.create(set_user)
-        session[:user_id] = @user.id
-        if params[:user][:password] == params[:user][:password_confirmation]
-          @user.password = params[:user][:password]
-        else
-          redirect_to 'login_path'
-        end
+    @user = User.create(set_user)
 
-      redirect_to 'root'
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to controller: 'welcome', action: 'home'
+
+    else
+      #  redirect_to new_user_path
+       redirect_to(controller: 'users', action: 'new')
+
     end
+
+
+    # if !params[:user][:name] || params[:user][:name] == ''
+    #   flash[:alert] = "Name must be proivided."
+    #   redirect_to 'login_path'
+    # else
+    #     @user = User.create(set_user)
+    #     session[:user_id] = @user.id
+    #     if params[:user][:password] == params[:user][:password_confirmation]
+    #       @user.password = params[:user][:password]
+    #     else
+    #       redirect_to 'login_path'
+    #     end
+
+    #   redirect_to 'root'
+    # end
   end
 
   private
